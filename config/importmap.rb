2630df7a22809632b71b607cf5bfb0dfb14e7b26
jcartronics <jcar-95@hotmail.com>
2023-09-10 01:56:40 -0300

    feat:(rich_text) Agrega texto enriquecido

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin 'jquery', to: 'jquery.js', preload: true
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
