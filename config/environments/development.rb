9d0fec7e14038440cf9586d3a12018874754f304
jcartronics <jcar-95@hotmail.com>
2023-09-10 01:06:29 -0300

    feat:(Devise) Agrega recuperar contraseña por mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,20 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@testemail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'example.com',
+  user_name: ENV["gmail_email"],
+  password: ENV["gmail_password"],
+  authentication: 'plain',
+  enable_starttls_auto: true
+  }
 end
